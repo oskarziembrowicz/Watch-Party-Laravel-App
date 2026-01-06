44d9368f9f067129dc1916fca07d2ae8a4edcb80
Oskar Ziembrowicz <rakson2002@gmail.com>
2026-01-06 12:28:15 +0100

    Application running and endpoint working

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/hello', function () {
+    return response()->json([
+        'message' => 'Hello from the laravel WatchParty'
+        ]);
+});
