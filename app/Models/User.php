8a929c4d9c2d5566ab4b11f57c457459728faca6
Oskar Ziembrowicz <rakson2002@gmail.com>
2026-02-01 18:21:09 +0100

    Set up laravel api for future auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
         'username',
